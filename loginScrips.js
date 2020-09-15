467c51e53d702857ff4dea53ba01967f76c7a620
Andres <aneduardotejada@gmail.com>
2020-09-15 10:33:58 -0500

    login

---
+++ loginScrips.js
@@ -0,0 +1,27 @@
+const fields = document.querySelectorAll('.textb input')
+const btn = document.querySelector('.btn')
+
+console.log(fields)
+console.log(btn)
+function check(){
+    if(fields[0].value != "" && fields[1].value != ""){
+        btn.disabled = false
+    }else{
+          btn.disabled = true
+    }
+}
+
+fields[0].addEventListener('keyup',check)
+fields[1].addEventListener('keyup',check)
+
+document.querySelector('.show-password').addEventListener('click',function(){
+    if(this.classList[2] =='fa-eye-slash'){
+        this.classList.remove('fa-eye-slash')
+        this.classList.add('fa-eye')
+        fields[1].type = "text"
+     }else{
+        this.classList.remove('fa-eye')
+        this.classList.add('fa-eye-slash')
+        fields[1].type = "password"
+     }
+})
